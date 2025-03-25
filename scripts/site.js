678bc4fe6144b4464a7c08cc82974d9ee8fc881b
Joshua Marino <108492036+joshuacmarino@users.noreply.github.com>
2025-03-25 12:21:42 -0700

    Initial commit

---
+++ scripts/site.js
@@ -0,0 +1,29 @@
+// Squarespace Template Javascript
+// Developed by Joshua Marino © 2025
+
+// CUSTOM FUNCTIONS
+
+
+// PAGE LOAD FUNCTIONS
+$(document).ready(function () {
+  
+  // Link Logo to Homepage Function
+  $('.logo').click(function(){ 
+    window.location = '../home';
+  });
+
+});
+
+// SQUARESPACE IMAGE LOADER
+function loadAllImages() {
+  var images = document.querySelectorAll('img[data-src]' );
+  for (var i = 0; i < images.length; i++) {
+    ImageLoader.load(images[i], {load: true});
+  }
+}
+
+// LOAD IMAGES ON PAGE LOAD
+document.addEventListener('DOMContentLoaded', loadAllImages);
+
+// RELOAD IMAGES ON WINDOW RESIZE
+window.addEventListener('resize', loadAllImages);
