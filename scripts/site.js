bf96def0a9a4fd4a1c3d262685d87a37ce7d346e
Joshua Marino <josha@joshas-macbook-pro.local>
2025-04-23 11:49:09 -0700

    f

--- scripts/site.js
+++ scripts/site.js
@@ -21,11 +21,11 @@ document.addEventListener("mouseup", (e) => cursor.classList.remove("click"));
 
 // Show custom cursor only over certain elements
 [document.querySelector('.home .collections .gallery-block'), document.querySelector('.home .collections .image-block'), document.querySelector('.footer .ctas')].forEach(item => {
-  item.addEventListener('mouseover', function () {
+  item?.addEventListener('mouseover', function () {
     cursor.classList.add('active');
   });
 
-  item.addEventListener('mouseout', function () {
+  item?.addEventListener('mouseout', function () {
     cursor.classList.remove('active');
   });
 
